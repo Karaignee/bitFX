e9a85be9687f2795847c5e4afb6df48596569689
Mark Ryder <ryddlemethis@gmail.com>
2014-04-26 13:30:53 +0100

    currency descrip

--- app/assets/javascripts/marketList.js
+++ app/assets/javascripts/marketList.js
@@ -1,8 +1,24 @@
 /**
  * Created by Mrs Ryder on 18/04/14.
  */
+var app = angular.module('app', []);
 
-function marketList($scope, $http){
+app.controller('currencyList', function ($scope, $http){
+    $scope.currency=[];
+
+    $scope.getCurrencies = function(){
+        $http({method : 'GET',
+            url : "https://openexchangerates.org/api/currencies.json?app_id=6334fc4be1884035840275ade9f84c9a"})
+            .success(function(data, status){
+                $scope.currency = data;
+            })
+            .error(function(data, status){
+                alert("error");
+            });
+    }
+});
+
+app.controller('marketList', function ($scope, $http){
     $scope.rates=[];
 
     $scope.getRates = function(){
@@ -13,7 +29,11 @@ function marketList($scope, $http){
        })
        .error(function(data, status){
         alert("error");
-
     });
-    }
 };
+});
+
+
+
+
+
